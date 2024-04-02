8b956af300814bcb37515d5a02241332fa03053c
GokulKailash <appankaali12@gmail.com>
2024-04-02 15:21:23 +0530

    msg1

---
+++ arith.py
@@ -0,0 +1,15 @@
+a=int(input("enter the first number : "));
+b=int(input("enter the second number : "));
+op=int(input("select 1)add 2)sub 3)mul 4)div 5)mod :"));
+if(op==1):
+    print(a+b);
+elif(op==2):
+    print(a-b);
+elif(op==3):
+    print(a*b);
+elif(op==4):
+    print(a/b);
+elif(op==5):
+    print(a%b);
+else:
+    print("choose any operation")
