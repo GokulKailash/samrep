80816c690d9bf4e6dc69768231a359f2da7bf2a6
Kaaligokul <147632538+Kaaligokul@users.noreply.github.com>
2024-04-25 23:01:59 +0530

    Add square operation

--- arith.py
+++ arith.py
@@ -1,6 +1,6 @@
 a=int(input("enter the first number : "));
 b=int(input("enter the second number : "));
-op=int(input("select 1)add 2)sub 3)mul 4)div 5)mod :"));
+op=int(input("select 1)add 2)sub 3)mul 4)div 5)mod 6)sqr :"));
 if(op==1):
     print(a+b);
 elif(op==2):
@@ -11,5 +11,7 @@ elif(op==4):
     print(a/b);
 elif(op==5):
     print(a%b);
+elif(op==6):
+    print(a**b);
 else:
     print("choose any operation")
